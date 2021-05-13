f7ee4bf6c958b7a3d13a97acecc74f796ad11b2e
duncte123 <contact@duncte123.me>
2021-05-13 10:24:04 +0200

    cool this is also possible

--- src/me/duncte123/killme/Main.java
+++ src/me/duncte123/killme/Main.java
@@ -4,11 +4,9 @@ import me.duncte123.killme.app.ToDoApp;
 
 import java.io.IOException;
 
-// This is the only class that will ever be used in the project
-// Java requires the main class to be a class :(
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+// Yes even this is an interface
+public interface Main {
+    static void main(String[] args) throws IOException {
         // yes this is how we are playing
         new ToDoApp() {}.start();
     }
